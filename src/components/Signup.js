de038b14cbf8a90b192bc8e1c46a448280d3aacd
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-29 22:47:05 +0100

    chakra ui setup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/button";
+import { FormControl } from "@chakra-ui/form-control";
+import { Heading, VStack } from "@chakra-ui/layout";
+import { useFormik } from "formik";
 
 const Signup = () => {
   return (
-      <div>Signup</div>
+      <VStack>
+          <Heading>Register a free account</Heading>
+          <p>Securely log into your cactus account</p>
+      </VStack>
   )
 };
 

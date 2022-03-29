5ef9b4557be0b7e78217f053faa61830ee06cc2d
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-29 12:47:55 +0100

    install react router, axios. setup page routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+//Pages
+import About from "./pages/About";
+import Auth from "./pages/Auth";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+import Faqs from "./pages/Faqs";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
-  return <div className="text-3xl font-bold underline">Cactus Insurance</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/faqs" element={<Faqs />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Auth />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="font-bold text-xl underline">Cactus Insurance</div>;
+};
+
+export default Home;

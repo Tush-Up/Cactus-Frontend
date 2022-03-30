0f87029f13608f4520040daefbe024f383c00355
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-30 12:46:37 +0100

    fix tailwind

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,62 @@
-import React from "react";
+import { Button, Heading, VStack, Text } from "@chakra-ui/react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import InputField from "./InputField";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={Yup.object({
+        email: Yup.string().required("Email is required"),
+        password: Yup.string()
+          .required("Password is required")
+          .min(7, "Password is too short"),
+      })}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
+    >
+      {(formik) => (
+        <VStack
+          as="form"
+          onSubmit={formik.handleSubmit}
+        >
+          <Heading as="h1" size="lg">
+            Log in to your account
+          </Heading>
+          <Text fontSize="md">Securely log into your cactus account</Text>
+
+          <InputField
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="enter email"
+          />
+
+          <InputField
+            label="Password"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button
+            width="100%"
+            type="submit"
+            variant="solid"
+            colorScheme="blackAlpha"
+          >
+            Log In
+          </Button>
+        </VStack>
+      )}
+    </Formik>
+  );
 };
 
 export default Login;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -4,8 +4,12 @@ import Signup from "../components/Signup";
 
 const Auth = () => {
   return (
-    <div>
-      <Signup />
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="italic">Cactus</div>
+      <div className="rounded shadow-lg w-auto h-auto p-8">
+        {window.location.pathname === "/login" && <Login />}
+        {window.location.pathname === "/register" && <Signup />}
+      </div>
     </div>
   );
 };

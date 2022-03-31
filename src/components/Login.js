ee289995158aa0d3da7eeff0553dff9c0c36fde3
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-31 23:39:17 +0100

    register and login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { Button, Heading, VStack, Text } from "@chakra-ui/react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import InputField from "./InputField";
+import axios from "../api/axios";
 
-const Login = () => {
+const Login = ({ user, setSuccessMsg, setErrorMsg }) => {
+  const LOGIN_URL = "/signIn";
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = localStorage.getItem("cactusUser");
+    console.log(getUser?.isLogin);
+    if (getUser?.isLogin) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
   return (
     <Formik
       initialValues={{
@@ -16,10 +29,37 @@ const Login = () => {
           .email("Invalid email"),
         password: Yup.string()
           .required("Password is required")
-          .min(7, "Password is too short"),
+          .min(8, "Password is too short"),
       })}
-      onSubmit={(values, actions) => {
-        console.log(values);
+      onSubmit={async (values, actions) => {
+        try {
+          const response = await axios.post(LOGIN_URL, values, {
+            headers: { "Content-Type": "application/json" },
+          });
+
+          setSuccessMsg("Successfully logged in!");
+          setTimeout(() => {
+            setSuccessMsg("");
+          }, 3000);
+
+          user = {
+            email: values.email,
+            isLogin: true,
+          };
+
+          localStorage.setItem("cactusUser", JSON.stringify(user));
+
+          setTimeout(() => {
+            navigate("/dashboard");
+          }, 3000);
+        } catch (error) {
+          if (error.response) {
+            setErrorMsg(error.response.data);
+            setTimeout(() => {
+              setErrorMsg("");
+            }, 7000);
+          }
+        }
         actions.resetForm();
       }}
     >
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,45 @@
 import React from "react";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
+import { Alert, AlertIcon } from "@chakra-ui/react";
+import { useState } from "react";
 
-const Auth = () => {
+const Auth = ({ setUser, user }) => {
+  const [successMsg, setSuccessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
   return (
     <div
-      className="w-full flex flex-col items-center justify-center py-8"
+      className="w-full flex flex-col items-center justify-center py-8 relative"
       style={{ minHeight: "100vh", height: "auto" }}
     >
+      <div
+        className={
+          successMsg || errorMsg
+            ? "fixed top-2 right-2 block"
+            : "fixed top-2 right-2 hidden"
+        }
+      >
+        <Alert status={successMsg ? "success" : "error"}>
+          <AlertIcon />
+          {successMsg}
+          {errorMsg}
+        </Alert>
+      </div>
+
       <div className="text-2xl font-bold mb-6">Cactus</div>
       <div className="rounded-3xl shadow-3xl w-auto h-auto p-8">
-        {window.location.pathname === "/login" && <Login />}
-        {window.location.pathname === "/register" && <Signup />}
+        {window.location.pathname === "/login" && (
+          <Login
+            setUser={setUser}
+            user={user}
+            setSuccessMsg={setSuccessMsg}
+            setErrorMsg={setErrorMsg}
+          />
+        )}
+        {window.location.pathname === "/register" && (
+          <Signup setSuccessMsg={setSuccessMsg} setErrorMsg={setErrorMsg} />
+        )}
       </div>
     </div>
   );

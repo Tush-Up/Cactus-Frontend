ae1c884f3c4d01ccd8073e04c8f15a116c182e27
James Harrison <somtochukwujames2000@gmail.com>
2022-04-04 13:58:00 +0100

    fix: changed sidebar hover and background color

---
+++ src/components/Activity/Activity.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Activity = () =>
+{
+    return (
+        <div className="w-72 h-screen bg-light-white shadow-xl p-5  pt-8">
+            <div>
+                <div className="flex gap-x-4 items-center">
+                    <img src="https://i.imgur.com/qX0QZQJ.png" alt="logo" className="cursor-pointer duration-500" />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Activity;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,14 @@ import cactuslogo from '../assets/cactuslogo.png';
 import { BsArrowLeftCircle, BsGrid, BsWallet2 } from 'react-icons/bs';
 import { AiOutlineUser } from 'react-icons/ai';
 import { CgBriefcase } from "react-icons/cg";
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 
 export default function Sidebar ( { open, setOpen } )
 {
-    
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+  
     return (
         <div className={ `${ open ? 'w-72' : 'w-20' } duration-300 h-screen bg-light-white shadow-xl p-5  pt-8 relative` } >
             <BsArrowLeftCircle className={ `absolute cursor-pointer rounded-full -right-3 top-9 w-7  ${ !open && 'rotate-180' }` } alt="logo"
@@ -28,7 +28,7 @@ export default function Sidebar ( { open, setOpen } )
             </div>
             <ul className="pt-6">
                 <Link to='/dashboard'>
-                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-dark-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard" ? 'bgg' : ''}` }>
+                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-light-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard" ? 'bgg' : ''}` }>
                         <BsGrid className={ `cursor-pointer duration-500` } size='30' />  <span className={ `${ !open && "hidden" } origin-left duration-200 text-lg` }>
                             Dashboard
                         </span>
@@ -36,21 +36,21 @@ export default function Sidebar ( { open, setOpen } )
                 </Link>
                 <Link to='/dashboard/plans'>
                     <li
-                        className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-dark-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/plans" ? 'bgg' : ''}` }>
+                        className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-light-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/plans" ? 'bgg' : ''}` }>
                         <CgBriefcase className={ `cursor-pointer duration-500` } size='30' /> <span className={ `${ !open && "hidden" } origin-left duration-200 text-lg` }>
                             Plans
                         </span>
                     </li>
                 </Link>
                 <Link to='/dashboard/claims'>
-                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-dark-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/claims" ? 'bgg' : ''}` }>
+                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-light-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/claims" ? 'bgg' : ''}` }>
                     <BsWallet2 className={ `cursor-pointer duration-500` } size='30' /> <span className={ `${ !open && "hidden" } origin-left duration-200 text-lg` }>
                         Claims
                     </span>
                     </li>
                 </Link>
                 <Link to='/dashboard/useraccount'>
-                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-dark-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/useraccount" ? 'bgg' : ''}` }>
+                    <li className={ `flex rounded-md p-2 cursor-pointer hover:bg-cactus-light-brown hover:text-white text-cactus-dark-brown text-sm items-center gap-x-4 mt-9 ${ window.location.pathname === "/dashboard/useraccount" ? 'bgg' : ''}` }>
                         <AiOutlineUser className={ `cursor-pointer duration-500` } size='30' /> <span className={ `${ !open && "hidden" } origin-left duration-200 text-lg` }>
                             Account
                         </span>

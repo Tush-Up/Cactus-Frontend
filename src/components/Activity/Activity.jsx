548d9f03b7477b1554e8c8b70c95f8add090fb43
James Harrison <somtochukwujames2000@gmail.com>
2022-04-12 11:39:17 +0100

    refactor : Added Real Values to the Activity Component

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -1,14 +1,54 @@
 import React from "react";
-import activity from '../../assets/activity.png';
+// import activity from '../../assets/activity.png';
+import profile from '../../assets/profile.png';
+import { BiBell } from 'react-icons/bi';
+import spiral from '../../assets/spiral.png';
 
 const Activity = () =>
 {
     return (
         <div className="bg-light-white">
             <div>
-                <h2> Activity </h2>
-                <img src={activity} alt="activity" /> {/*Here  I added only the activity image from the figma file
-                                                         for presentation sake, will gently work with tomorrow as soon as i charge my devices*/ }
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                        <div className="border-4 border-cactus-light-brown rounded-full p-1">
+                            <img src={ profile } alt='profileImage'/>
+                        </div>
+                        <h2 className="font-bold text-xl">Name</h2>
+                    </div>
+                    <div className="rounded-full p-4 bg-light-white shadow-lg z-20">
+                        <BiBell size='40' />
+                    </div>
+                </div>
+                <div className="rounded-md shadow-lg z-20 p-8 bg-light-white mt-8">
+                        <h2>Upcoming Payments</h2>
+                </div>
+                <div className='border-b-4 border-gray-200 h-10'></div>
+                <div className="rounded-md shadow-lg z-20 p-8 bg-light-white mt-8 mb-10">
+                    <div className='flex items-center gap-2'>
+                        <img src={ profile } alt='profileImage' />
+                        <h2 className="font-bold text-xl">Name</h2>
+                    </div>
+
+                    
+                </div>
+                <div className="rounded-md shadow-lg z-20 p-2 bg-light-white ">
+                    <div className="rounded-md bg-cactus-light-brown p-5 flex">
+                        <div>
+                            <h2 className="text-white text-xl font-bold">Do You Still Need Our Help?</h2>
+                            <p className="text-xs text-white">Send your request via email</p>
+                            <a href="https://github.com/">
+                                <button className="bg-light-white rounded-md p-3 mt-2" >
+                                    <h3 className='text-cactus-light-brown font-bold'>Send Request</h3>
+                                </button>
+                            </a>
+                        </div>
+                        {/* <div className="absolute bg-cactus-light-brown">
+                            <img src={ spiral } alt='spiralImage' />
+                        </div> */}
+                    </div>
+                </div>
+
             </div>
 
         </div>

42d97fb4c16e02c3339eb485f5f676281f667db6
James Harrison <somtochukwujames2000@gmail.com>
2022-04-12 13:00:21 +0100

    feature : added rating component and it's updating rating values

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -2,24 +2,27 @@ import React, {useState} from "react";
 import Calendar from 'react-calendar';
 import profile from '../../assets/profile.png';
 import { BiBell } from 'react-icons/bi';
+import StarRating from "../StarRating";
 // import spiral from '../../assets/spiral.png';
 
 
 const Activity = () =>
 {
-    const [ date, setDate ] = useState( new Date() )
-        ;
+    const [ date, setDate ] = useState( new Date() );
+    const [ log, setLog ] = useState( 0 );
+
+
     return (
         <div className="bg-light-white">
             <div>
                 <div className="flex justify-between">
                     <div className="flex items-center gap-2">
-                        <div className="border-4 border-cactus-light-brown rounded-full p-1">
+                        <div className="border-4 border-cactus-light-brown rounded-full p-1 cursor-pointer">
                             <img src={ profile } alt='profileImage'/>
                         </div>
                         <h2 className="font-bold text-xl">Name</h2>
                     </div>
-                    <div className="rounded-full p-4 bg-light-white shadow-lg z-20">
+                    <div className="rounded-full p-4 bg-light-white shadow-lg z-20 cursor-pointer">
                         <BiBell size='40' />
                     </div>
                 </div>
@@ -35,8 +38,33 @@ const Activity = () =>
                         <img src={ profile } alt='profileImage' />
                         <h2 className="font-bold text-xl">Name</h2>
                     </div>
+                    <div className='flex items-center justify-between gap-2 mt-4'>
+                        <StarRating onChange={ setLog} /> {/* Rating Components will be here */}
+                        <h2 className="font-bold text-3xl">{log} <span className="text-xs text-gray-400">out of 5 stars</span></h2>
+                    </div>
+                    <div>
+                        <div className='flex items-center justify-between mt-3'>
+                            <h2>Excellent</h2>
+                            <h3>35</h3>
+                        </div>
+                        <div className='flex items-center justify-between mt-3'>
+                            <h2>Good</h2>
+                            <h3>25</h3>
+                        </div>
+                        <div className='flex items-center justify-between mt-3'>
+                            <h2>Average</h2>
+                            <h3>20</h3>
 
-                    
+                        </div>
+                        <div className='flex items-center justify-between mt-3'>
+                            <h2>Avg Below</h2>
+                            <h3>15</h3>
+                        </div>
+                        <div className='flex items-center justify-between mt-3'>
+                            <h2>Poor</h2>
+                            <h3>05</h3>
+                        </div>
+                    </div>
                 </div>
                 <div className="rounded-md shadow-lg z-20 p-2 bg-light-white ">
                     <div className="rounded-md bg-cactus-light-brown p-5 flex">
@@ -44,7 +72,7 @@ const Activity = () =>
                             <h2 className="text-white text-xl font-bold">Do You Still Need Our Help?</h2>
                             <p className="text-xs text-white">Send your request via email</p>
                             <a href="https://github.com/">
-                                <button className="bg-light-white rounded-md p-3 mt-2" >
+                                <button className="bg-light-white rounded-md p-4 mt-2" >
                                     <h3 className='text-cactus-light-brown font-bold'>Send Request</h3>
                                 </button>
                             </a>
---
+++ src/components/StarRating.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Star from "./Star";
+function StarRating ( { onChange } )
+{
+    const [ rating, setRating ] = useState( 0 );
+    const changeRating = ( newRating ) =>
+    {
+        setRating( newRating );
+        onChange?.( newRating );
+    };
+    return (
+        <span className="flex text-2xl items-center">
+            { [ 1, 2, 3, 4, 5 ].map( ( value ) => (
+                <Star
+                    key={ value }
+                    filled={ value <= rating }
+                    onClick={ () => changeRating( value ) }
+                />
+            ) ) }
+        </span>
+    );
+}
+export default StarRating;

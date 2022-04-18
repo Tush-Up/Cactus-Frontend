d22287eeef68acb3aca5e434925f78203cdcd259
Iheyinwa <caruogu@Outlook.com>
2022-04-18 09:04:19 -0700

    ft home Hifi2

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,18 +1,20 @@
 export default function HomeHeader({ photo, text1, text2, paragraph1, paragraph2, paragraph3 }) {
 	return (
-		<header className="text-center text-white">
-			<img src={photo} alt="" className="max-w-[50%] rounded-3xl h-screen" />
-			<p className="font-bold text-5xl pt-40 mx-60 w-3/5 ">
-				{text1} <br />
-				{text2}
-			</p>
-			<p className="font-bold mx-48 pt-20">
-				{paragraph1}
-				<br />
-				{paragraph2}
-				<br />
-				{paragraph3}
-			</p>
+		<header className="text-center text-white -z-10">
+			<img src={photo} alt="" className="w-full rounded-3xl h-screen" />
+			<div className="-mt-[53.7%]">
+				<p className="font-bold text-6xl pt-40 mx-64 w-3/5 ">
+					{text1} <br />
+					{text2}
+				</p>
+				<p className="font-medium mx-48 pt-12 text-xl">
+					{paragraph1}
+					<br />
+					{paragraph2}
+					<br />
+					{paragraph3}
+				</p>
+			</div>
 		</header>
 	);
 }

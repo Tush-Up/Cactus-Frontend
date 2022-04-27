f8b1709cc05789b9a9418afb5521af468c3642ab
Iheyinwa <caruogu@Outlook.com>
2022-04-27 00:45:48 -0700

    ft home HiFi 2

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,13 @@
-import { CheckedStar, Comment, LeftArrow, RightArrow, UncheckedStar } from '../svg/icons';
+import { CheckedStar, Comment, UncheckedStar } from '../svg/icons';
 import ReviewCard from './ReviewCard';
 import ReviewHeader from './ReviewHeader';
+import Klaus from '../img/Klaus Fischer.jpg';
+import Harriet from '../img/Harriet Nwagwu.jpg';
+import Audu from '../img/Audu Umaru.jpg';
 
 export default function Review() {
 	return (
-		<section className="relative top-[24rem]">
+		<section className="relative top-[24rem] mb-[36rem]">
 			<ReviewHeader
 				text1="What are our clients"
 				text2="saying about us?"
@@ -19,7 +22,7 @@ export default function Review() {
         I’ve lost my job a lot before I found a stable one, but before that
          Cactus was really there for me in those difficult times. 
          I will always recommend Cactus to my collegues."
-					// background="url('/src/img/Klaus Fischer.jpg')"
+					photo={Klaus}
 					client="Klaus Fischer"
 					job="Lawyer Lami and Sons"
 					rating1={<CheckedStar />}
@@ -32,6 +35,7 @@ export default function Review() {
 					text="I could remember last year a friend recommended cactus to me,
 					 then I suddenly lost my job. I was so broke. Thank God for Cactus, 
 					 I was able to stay on my feet and pay my bills while looking for a new job."
+					photo={Harriet}
 					client="Harriet Nwagwu"
 					job="Banker, UBA"
 					rating1={<CheckedStar />}
@@ -41,9 +45,11 @@ export default function Review() {
 					rating5={<CheckedStar />}
 				/>
 				<ReviewCard
-					text="Trusting Cactus to secure my job
-					is the best decision I’ve ever made.
+					text="Subscribing to Cactus Job Insurance company to
+					secure my stability during job hunting
+					is the best decision I made. The transactions are done seamlessly.
 					 Cactus is indeed true to her goals and objectives."
+					photo={Audu}
 					client="Audu Umaru"
 					job="Banker, UBA"
 					rating1={<CheckedStar />}
@@ -52,15 +58,6 @@ export default function Review() {
 					rating4={<CheckedStar />}
 					rating5={<UncheckedStar />}
 				/>
-			</div>
-			<div className="flex justify-between mt-8">
-				<div>
-					<p>buttons</p>
-				</div>
-				<div className="flex mr-12 hover:cursor-pointer">
-					<LeftArrow />
-					<RightArrow />
-				</div>
 			</div>
 		</section>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      boxShadow: {
-        "3xl": "0px 104px 80px 32px rgba(33, 33, 33, 0.03)",
-      },
-      colors: {
-        "black": "#212121",
-        "light-white": "#ffffff",
-        "cactus-brown": "#B38B59",
-      },
-      backgroundImage: {
-        auth: "url('/src/assets/auth-bg.png')",
-      },
-    },
-  },
-  plugins: [],
+	content: [ './src/**/*.{js,jsx,ts,tsx}' ],
+	theme: {
+		extend: {
+			boxShadow: {
+				'3xl': '0px 104px 80px 32px rgba(33, 33, 33, 0.03)'
+			},
+			colors: {
+				black: '#212121',
+				'light-white': '#ffffff',
+				'cactus-brown': '#B38B59'
+			},
+			backgroundImage: {
+				auth: "url('/src/assets/auth-bg.png')",
+				faqs: "url('/src/img/FAQS.jpg')"
+			}
+		}
+	},
+	plugins: []
 };
